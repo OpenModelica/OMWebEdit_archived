3aec29f48b100590f239662ff5811108efee7186
Anurag Kapur <anurag@beancrunch.com>
2020-06-23 16:53:01 +0100

    Update app to a hello-world react-diagrams UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import createEngine, {
+  DefaultLinkModel,
+  DefaultNodeModel,
+  DiagramModel,
+} from "@projectstorm/react-diagrams";
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //1) setup the diagram engine
+  var engine = createEngine();
+
+  //2) setup the diagram model
+  var model = new DiagramModel();
+
+  //3-A) create a default node
+  var node1 = new DefaultNodeModel({
+    name: "Node 1",
+    color: "rgb(0,192,255)",
+  });
+  node1.setPosition(100, 100);
+  let port1 = node1.addOutPort("Out");
+
+  //3-B) create another default node
+  var node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
+  let port2 = node2.addInPort("In");
+  node2.setPosition(400, 100);
+
+  // link the ports
+  let link1 = port1.link<DefaultLinkModel>(port2);
+  link1.getOptions().testName = "Test";
+  link1.addLabel("Hello World!");
+
+  //4) add the models to the root graph
+  model.addAll(node1, node2, link1);
+
+  //5) load model into engine
+  engine.setModel(model);
+  return <CanvasWidget engine={engine} className="canvas" />;
 }
 
 export default App;
