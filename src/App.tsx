5074cdb5681d7db978f3eddcbb15ed4ca8ea0e09
Anurag Kapur <anurag@beancrunch.com>
2020-06-24 17:56:25 +0100

    Adjust resistor component styling
    - remove top and botton ports
    - use svg without padding around core vector data

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import createEngine, {
 } from "@projectstorm/react-diagrams";
 import { CanvasWidget } from "@projectstorm/react-canvas-core";
 import "./App.css";
-import {SimplePortFactory} from "./components/resistor/SimplePortFactory";
+import {SimplePortFactory} from "./components/common/SimplePortFactory";
 import {ResistorPortModel} from "./components/resistor/ResistorPortModel";
 import {ResistorNodeFactory} from "./components/resistor/ResistorNodeFactory";
 import {ResistorNodeModel} from "./components/resistor/ResistorNodeModel";
@@ -42,20 +42,8 @@ function App() {
   var link1 = port1.link(node2.getPort(PortModelAlignment.LEFT));
   var link2 = port3.link(node2.getPort(PortModelAlignment.RIGHT));
 
-  var node4 = new DefaultNodeModel('Node 4', 'rgb(0,192,255)');
-  var port4 = node4.addOutPort('Out');
-  node4.setPosition(200, 10);
-
-  var link3 = port4.link(node2.getPort(PortModelAlignment.TOP));
-
-  var node5 = new DefaultNodeModel('Node 5', 'mediumpurple');
-  var port5 = node5.addInPort('In');
-  node5.setPosition(400, 300);
-
-  var link4 = port5.link(node2.getPort(PortModelAlignment.BOTTOM));
-
   //4) add the models to the root graph
-  model.addAll(node1, node2, node3, link1, link2, node4, link3, link4, node5);
+  model.addAll(node1, node2, node3, link1, link2);
 
   //5) load model into engine
   engine.setModel(model);
--- src/components/resistor/ResistorNodeWidget.tsx
+++ src/components/resistor/ResistorNodeWidget.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { ResistorNodeModel } from './ResistorNodeModel';
 import { DiagramEngine, PortModelAlignment, PortWidget } from '@projectstorm/react-diagrams';
 import styled from '@emotion/styled';
+import ReactLogo from './../../logo.svg';
+import ResistorIcon from './Resistor-icon.svg';
 
 export interface ResistorNodeWidgetProps {
 	node: ResistorNodeModel;
@@ -33,34 +35,7 @@ export class ResistorNodeWidget extends React.Component<ResistorNodeWidgetProps>
 					width: this.props.size,
 					height: this.props.size
 				}}>
-				<svg
-					width={this.props.size}
-					height={this.props.size}
-					dangerouslySetInnerHTML={{
-						__html:
-							`
-          <g id="Layer_1">
-          </g>
-          <g id="Layer_2">
-            <polygon fill="mediumpurple" stroke="${
-							this.props.node.isSelected() ? 'white' : '#000000'
-						}" stroke-width="3" stroke-miterlimit="10" points="10,` +
-							this.props.size / 2 +
-							` ` +
-							this.props.size / 2 +
-							`,10 ` +
-							(this.props.size - 10) +
-							`,` +
-							this.props.size / 2 +
-							` ` +
-							this.props.size / 2 +
-							`,` +
-							(this.props.size - 10) +
-							` "/>
-          </g>
-        `
-					}}
-				/>
+				<img src={ResistorIcon} alt="Resistor Icon" width="100%"/>
 				<PortWidget
 					style={{
 						top: this.props.size / 2 - 8,
@@ -73,31 +48,11 @@ export class ResistorNodeWidget extends React.Component<ResistorNodeWidgetProps>
 				</PortWidget>
 				<PortWidget
 					style={{
-						left: this.props.size / 2 - 8,
-						top: -8,
-						position: 'absolute'
-					}}
-					port={this.props.node.getPort(PortModelAlignment.TOP)}
-					engine={this.props.engine}>
-					<Port />
-				</PortWidget>
-				<PortWidget
-					style={{
 						left: this.props.size - 8,
 						top: this.props.size / 2 - 8,
 						position: 'absolute'
 					}}
 					port={this.props.node.getPort(PortModelAlignment.RIGHT)}
-					engine={this.props.engine}>
-					<Port />
-				</PortWidget>
-				<PortWidget
-					style={{
-						left: this.props.size / 2 - 8,
-						top: this.props.size - 8,
-						position: 'absolute'
-					}}
-					port={this.props.node.getPort(PortModelAlignment.BOTTOM)}
 					engine={this.props.engine}>
 					<Port />
 				</PortWidget>
