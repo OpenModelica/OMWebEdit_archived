75fc2cd5e4e98d54540d154fe6237bbb2d82cb5b
Anurag Kapur <anurag@beancrunch.com>
2020-06-25 11:15:14 +0100

    Ensure custom ports are set during serialization

--- src/App.tsx
+++ src/App.tsx
@@ -23,56 +23,56 @@ function App() {
   // setup the diagram model
   const model = new DiagramModel();
 
-  // setup nodes
-  const voltageNode = new OMWebEditDefaultNodeModel("sine-voltage", "clockwise-90");
-  voltageNode.setPosition(100, 200);
+  // // setup nodes
+  // const voltageNode = new OMWebEditDefaultNodeModel("sine-voltage", "clockwise-90");
+  // voltageNode.setPosition(100, 200);
+  //
+  // const resistorNode = new OMWebEditDefaultNodeModel("resistor", "default");
+  // resistorNode.setPosition(300, 100);
+  //
+  // const inductorNode = new OMWebEditDefaultNodeModel("inductor", "default");
+  // inductorNode.setPosition(600, 100);
+  //
+  // let ports: PortModelAlignment[]  = [PortModelAlignment.TOP];
+  // const groundNode = new OMWebEditDefaultNodeModel("ground", "default", ports);
+  // groundNode.setPosition(300, 400);
+  //
+  // // setup links
+  // const resistorLeftPort = resistorNode.getPort(PortModelAlignment.LEFT);
+  // const resistorRightPort = resistorNode.getPort(PortModelAlignment.RIGHT);
+  // const inductorLeftPort = inductorNode.getPort(PortModelAlignment.LEFT);
+  // const inductorRightPort = inductorNode.getPort(PortModelAlignment.RIGHT);
+  // const groundTopPort = groundNode.getPort(PortModelAlignment.TOP);
+  // const voltageLeftPort = voltageNode.getPort(PortModelAlignment.LEFT);
+  // const voltageRightPort = voltageNode.getPort(PortModelAlignment.RIGHT);
+  //
+  // const resistorInductorLink = resistorLeftPort.createLinkModel();
+  // resistorInductorLink.setSourcePort(resistorRightPort);
+  // resistorInductorLink.setTargetPort(inductorLeftPort);
+  //
+  // const inductorGroundLink = inductorRightPort.createLinkModel();
+  // inductorGroundLink.setSourcePort(inductorRightPort);
+  // inductorGroundLink.setTargetPort(groundTopPort);
+  //
+  // const voltageResistorLink = voltageLeftPort.createLinkModel();
+  // voltageResistorLink.setSourcePort(voltageLeftPort);
+  // voltageResistorLink.setTargetPort(resistorLeftPort);
+  //
+  // const voltageGroundLink = voltageRightPort.createLinkModel();
+  // voltageGroundLink.setSourcePort(voltageRightPort);
+  // voltageGroundLink.setTargetPort(groundTopPort);
+  //
+  // // add everything to model
+  // model.addAll(voltageNode, resistorNode, inductorNode, groundNode);
+  // model.addAll(resistorInductorLink, inductorGroundLink, voltageResistorLink, voltageGroundLink);
+  //
+  // // load model into engine
+  // engine.setModel(model);
 
-  const resistorNode = new OMWebEditDefaultNodeModel("resistor", "default");
-  resistorNode.setPosition(300, 100);
-
-  const inductorNode = new OMWebEditDefaultNodeModel("inductor", "default");
-  inductorNode.setPosition(600, 100);
-
-  let ports: PortModelAlignment[]  = [PortModelAlignment.TOP];
-  const groundNode = new OMWebEditDefaultNodeModel("ground", "default", ports);
-  groundNode.setPosition(300, 400);
-
-  // setup links
-  const resistorLeftPort = resistorNode.getPort(PortModelAlignment.LEFT);
-  const resistorRightPort = resistorNode.getPort(PortModelAlignment.RIGHT);
-  const inductorLeftPort = inductorNode.getPort(PortModelAlignment.LEFT);
-  const inductorRightPort = inductorNode.getPort(PortModelAlignment.RIGHT);
-  const groundTopPort = groundNode.getPort(PortModelAlignment.TOP);
-  const voltageLeftPort = voltageNode.getPort(PortModelAlignment.LEFT);
-  const voltageRightPort = voltageNode.getPort(PortModelAlignment.RIGHT);
-
-  const resistorInductorLink = resistorLeftPort.createLinkModel();
-  resistorInductorLink.setSourcePort(resistorRightPort);
-  resistorInductorLink.setTargetPort(inductorLeftPort);
-
-  const inductorGroundLink = inductorRightPort.createLinkModel();
-  inductorGroundLink.setSourcePort(inductorRightPort);
-  inductorGroundLink.setTargetPort(groundTopPort);
-
-  const voltageResistorLink = voltageLeftPort.createLinkModel();
-  voltageResistorLink.setSourcePort(voltageLeftPort);
-  voltageResistorLink.setTargetPort(resistorLeftPort);
-
-  const voltageGroundLink = voltageRightPort.createLinkModel();
-  voltageGroundLink.setSourcePort(voltageRightPort);
-  voltageGroundLink.setTargetPort(groundTopPort);
-
-  // add everything to model
-  model.addAll(voltageNode, resistorNode, inductorNode, groundNode);
-  model.addAll(resistorInductorLink, inductorGroundLink, voltageResistorLink, voltageGroundLink);
-
-  // load model into engine
-  engine.setModel(model);
-
-  // const model2 = new DiagramModel();
-  // const str = JSON.stringify(modelJson);
-  // model2.deserializeModel(JSON.parse(str), engine);
-  // engine.setModel(model2);
+  const model2 = new DiagramModel();
+  const str = JSON.stringify(modelJson);
+  model2.deserializeModel(JSON.parse(str), engine);
+  engine.setModel(model2);
 
   return (
       <div>
