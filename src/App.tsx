913bf65f47db8b60d0fe54b77701f597caa89e37
Anurag Kapur <anurag@beancrunch.com>
2020-07-28 14:33:30 +0100

    Constructor ground and resistor nodes from canned server response

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import "./App.css";
 import { OMComponentFactory } from "./custom-nodes/omcomponent/OMComponentFactory";
 import { OMComponentModel } from "./custom-nodes/omcomponent/OMComponentModel";
 import inductorJson from "./component-inductor.json";
+import resistorJson from "./component-resistor.json";
+import groundJson from "./component-ground.json";
 import { OMPort } from "./domain-model/OMPort";
 
 function getNodeFromServerResponse(nodeJson) {
@@ -20,11 +22,18 @@ function App() {
   const engine = createEngine();
   engine.getNodeFactories().registerFactory(new OMComponentFactory());
   const model = new DiagramModel();
+
   const node1 = getNodeFromServerResponse(inductorJson);
   node1.setPosition(0, 50);
 
+  const node2 = getNodeFromServerResponse(resistorJson);
+  node2.setPosition(0, 250);
+
+  const node3 = getNodeFromServerResponse(groundJson);
+  node3.setPosition(0, 450);
+
   // add everything to model
-  model.addAll(node1);
+  model.addAll(node1, node2, node3);
 
   // load model into engine
   engine.setModel(model);
